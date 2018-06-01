faaae6f4ed09764a6d43bc5bacdabb8b45e5d930
kjd9xhp <39095464+kjd9xhp@users.noreply.github.com>
2018-06-01 22:55:12 +0700

    khởi tạo

---
+++ app.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var express_handlebars_sections = require('express-handlebars-sections');
+var bodyParser = require('body-parser');
+var path = require('path');
+var wnumb = require('wnumb');
+
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+
+var handleLayoutMDW = require('./middle-wares/handleLayout'),
+    handle404MDW = require('./middle-wares/handle404');
+
+var homeController = require('./controllers/homeController'),
+    categoryController = require('./controllers/categoryController'),
+    productController = require('./controllers/productController'),
+    accountController = require('./controllers/accountController');
+
+var app = express();
+
+var upload = require('express-fileupload');
+
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: 'views/_layouts/',
+    helpers: {
+        section: express_handlebars_sections(),
+        number_format: n => {
+            var nf = wnumb({
+                thousand: ','
+            });
+            return nf.to(n);
+        }
+    }
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+//
+// session
+
+var sessionStore = new MySQLStore({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'phuoc123',
+    database: 'qlsach',
+    createDatabaseTable: true,
+    schema: {
+        tableName: 'sessions',
+        columnNames: {
+            session_id: 'session_id',
+            expires: 'expires',
+            data: 'data'
+        }
+    }
+});
+
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(handleLayoutMDW);
+
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+app.use('/home', homeController);
+app.use('/category', categoryController);
+app.use('/product', productController);
+app.use('/account', accountController);
+
+app.use(handle404MDW);
+
+app.use(upload()); // configure middleware
+
+app.listen(3000, () => {
+    console.log('Site running on port 3000');
+});
