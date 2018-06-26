9ba1afcdc09dd307217b76feeb09d98200850007
kjd9xhp <39095464+kjd9xhp@users.noreply.github.com>
2018-06-26 12:27:51 +0700

    sửa cartrepo

--- repos/cartRepo.js
+++ repos/cartRepo.js
@@ -10,15 +10,10 @@ exports.add = (cart, item) => {
 
     cart.push(item);
 }
-
-<<<<<<< HEAD
 exports.add1 = items => {
     var sql = `insert into orderdetails(OrderID, ProID, Quantity, Price, Amount) values('${items[0].Amount}', '${items[0].Amount}', '${items[0].Amount}', '${items[0].Amount}', '${items[0].Amount}')`;
     return db.save(sql);
 }
-
-=======
->>>>>>> 82f8110391db4f65ffe3ba81d86650a5c1e1e300
 exports.remove = (cart, proId) => {
     for (var i = cart.length - 1; i >= 0; i--) {
         if (proId === cart[i].ProId) {
